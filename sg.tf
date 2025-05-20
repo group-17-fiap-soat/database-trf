9ea378cc90052877532cc1697684b6f9eab7d84f
Manocchio <guruu.leste@gmail.com>
2025-05-20 01:49:07 -0300

    fix var

--- sg.tf
+++ sg.tf
@@ -1,3 +1,27 @@
+resource "aws_security_group" "sg" {
+  name        = "SG-allow-all"
+  description = "Usado no EKS com 6/7SOAT"
+  vpc_id      = var.vpc_id
+
+  # Inbound
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound
+  egress {
+    description = "All"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "sg_postgres" {
   name        = "SG-postgres"
   description = "Permite acesso ao PostgreSQL (porta 5432)"
