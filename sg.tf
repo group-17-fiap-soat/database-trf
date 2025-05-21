6b8edd40d17cf3210d03d9703bc91e06e621918c
Manocchio <guruu.leste@gmail.com>
2025-05-20 22:24:10 -0300

    remove sg that are not related to the database

--- sg.tf
+++ sg.tf
@@ -1,27 +1,3 @@
-resource "aws_security_group" "sg" {
-  name        = "SG-allow-all"
-  description = "Usado no EKS com 6/7SOAT"
-  vpc_id      = data.aws_vpc.selected.id
-
-  # Inbound
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound
-  egress {
-    description = "All"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "sg_postgres" {
   name        = "SG-postgres"
   description = "Permite acesso ao PostgreSQL (porta 5432)"
